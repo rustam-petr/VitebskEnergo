349678191959f03b63f4baa85a0b5e6510d7c48c
rustam-petr <74614737+rustam-petr@users.noreply.github.com>
2022-09-27 13:03:57 +0300

    +

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 include "config.php";
 include "function.php";
 
-$tests = get_topic();
-print_r($tests);
+$topics = get_topic();
+print_r($topics);
 
 
 ?>
@@ -14,17 +14,27 @@ print_r($tests);
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 <body>
 <?php
-if ($tests) {
+if ($topics) {
     ?>
-    <h3> Выберите тему</h3>
-    <?php foreach ($tests as $test) { ?>
-        <p><a href="?id=<?= $test['id'] ?>"><?= $test['topic'] ?></a></p>
+<div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+       Выберите тему
+    </button>
+    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+    <?php foreach ($topics as $topic) { ?>
+        <li><a href="?id=<?= $topic['id'] ?>"><?= $topic['topic'] ?></a></li>
+<!--        <p><a href="?id=--><?//= $topic['id'] ?><!--">--><?//= $topic['topic'] ?><!--</a></p>-->
     <?php } ?>
 <?php } ?>
+
+
+
 
 </body>
 </html>
